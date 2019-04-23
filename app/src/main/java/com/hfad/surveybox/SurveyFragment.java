069040e84b78ffc3a0418c8e30541fac2880f653
Git-NS-Hub <46581114+Git-NS-Hub@users.noreply.github.com>
2019-04-23 12:51:03 -0600

    attempting to clean up code for more commits 2

--- app/src/main/java/com/hfad/surveybox/SurveyFragment.java
+++ app/src/main/java/com/hfad/surveybox/SurveyFragment.java
@@ -1,15 +1,11 @@
 package com.hfad.surveybox;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-
 
 public class SurveyFragment extends Fragment {
-
 
     public SurveyFragment() {
         // Required empty public constructor
@@ -18,7 +14,6 @@ public class SurveyFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_survey, container, false);
-
         return inflater.inflate(R.layout.fragment_survey, container, false);
     }
 

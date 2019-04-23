902ffde25a5bfcf8123bc973a1ca03a9a6cb5ef4
Git-NS-Hub <46581114+Git-NS-Hub@users.noreply.github.com>
2019-04-23 13:59:20 -0600

    new fragment made and lock and unlock buttons should now work

--- app/src/main/java/com/hfad/surveybox/SettingsActivity.java
+++ app/src/main/java/com/hfad/surveybox/SettingsActivity.java
@@ -1,13 +1,29 @@
 package com.hfad.surveybox;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SettingsActivity extends AppCompatActivity {
+    private Button lockButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        lockButton = findViewById(R.id.lockButton);
+        lockButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new SurveyFragment();
+                moveToFragment(fragment);
+            }
+        });
+    }
+    private void moveToFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.frameLayout, fragment, fragment.getClass().getSimpleName()).addToBackStack(null).commit();
     }
 }

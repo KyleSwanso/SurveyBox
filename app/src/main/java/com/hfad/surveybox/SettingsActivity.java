98be27c885bbbe2a6bf882782284f4ff4617f3ee
Git-NS-Hub <46581114+Git-NS-Hub@users.noreply.github.com>
2019-04-24 16:34:35 -0600

    lock button has been adjusted and activity_setting is now the landing page

--- app/src/main/java/com/hfad/surveybox/SettingsActivity.java
+++ app/src/main/java/com/hfad/surveybox/SettingsActivity.java
@@ -1,29 +1,26 @@
 package com.hfad.surveybox;
 
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 public class SettingsActivity extends AppCompatActivity {
-    private Button lockButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        lockButton = findViewById(R.id.lockButton);
-        lockButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Fragment fragment = new SurveyFragment();
-                moveToFragment(fragment);
-            }
-        });
-    }
-    private void moveToFragment(Fragment fragment) {
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.frameLayout, fragment, fragment.getClass().getSimpleName()).addToBackStack(null).commit();
+
+        Button LockButton = (Button) findViewById(R.id.lockButton);
+
+        LockButton.setOnClickListener(new View.OnClickListener() {
+                                          @Override
+                                          public void onClick(View v) {
+                                              startActivity(new Intent(SettingsActivity.this, SurveyActivity.class));
+                                          }
+                                      }
+        );
     }
 }
